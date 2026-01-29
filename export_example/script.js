6046ea122f19f91c10a5fa59527f937ad881e9ef
lomas-chaudhary <lomas.chaudhary.99@gmail.com>
2026-01-29 18:55:41 +0530

    3. export_example : named and default

--- export_example/script.js
+++ export_example/script.js
@@ -1,5 +1,13 @@
-import username from './app.js';
+import username from './app.js'
 import {arr} from './app.js'
+import naam from './example_two.js'
+import { age} from './example_two.js';
+import { skills } from './example_two.js';
 
 console.log(username);
 console.log(arr)
+
+console.log("example two")
+console.log(age)
+console.log(skills)
+console.log(naam)
