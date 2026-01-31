fa646ddc07ef222e7421f71503ff4f515fff46cb
lomas-chaudhary <lomas.chaudhary.99@gmail.com>
2026-01-31 17:20:10 +0530

    7. 03-components : understanding component based architecture

---
+++ 03-components/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='card'>
+      <h1> Lomas chaudhary</h1>
+      <h2> Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore sunt perspiciatis quaerat id, atque error sed eligendi et rem magni dolorem, odit omnis facere mollitia quia, est deleniti ipsum totam! </h2>
+    </div>
+  )
+}
+
+export default Card
---
+++ 03-components/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Card from '../components/Card'
+import Navbar from '../components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+
+    </div>
+  )
+}
+
+export default App
